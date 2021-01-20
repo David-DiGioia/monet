dc085c09f531375c4b72eb2a790c426afdea2be7
David DiGioia <davidofjoy@gmail.com>
2021-01-20 13:39:19 -0500

    add tracy profiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <vk_engine.h>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+#ifdef TRACY_ENABLE
+	std::cout << "TRACY_ENABLE is defined\n";
+#endif
+
 	VulkanEngine engine;
 
 	engine.init();
