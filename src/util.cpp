a76f63f04bc27d3abb5332e7c157233447319cd6
David DiGioia <davidofjoy@gmail.com>
2021-05-19 01:45:19 -0400

    derivation comment

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,24 @@
 #include "util.h"
 
+/*
+* Derivation:
+* since -z is in front of camera and depth is in range [0, 1]
+* 
+* We have ax + b.
+* m is the far plane.
+* n is the near plane.
+* The far plane should map to a depth of 1, and the near plane to a depth of 0.
+* So then:
+* 
+* -am + b = 1
+* -an + b = 0
+* 
+* -am + an = 1
+* -a(m-n) = 1
+* a = -1 / (m-n)
+* 
+* b = an = -n / (m-n)
+*/
 glm::mat4 ortho(float left, float right, float bottom, float top, float zNear, float zFar)
 {
 	glm::mat4 result{ 1 };
